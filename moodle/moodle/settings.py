4af66065ddfe68d90c182904514b99bfa63f2c32
Victor-Alexandru <victor21cuciureanu@gmail.com>
2020-02-26 18:51:06 +0200

    command added

--- moodle/moodle/settings.py
+++ moodle/moodle/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = ')fjq4zu_v+$a+v@vh*43(k=54aym=njslqrx$cypw1&--#6hjl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.102', '192.168.1.105']
+ALLOWED_HOSTS = ['192.168.1.102', '192.168.1.105', '192.168.1.103']
 
 # Application definition
 
