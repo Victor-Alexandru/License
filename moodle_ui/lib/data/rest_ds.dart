696382b342eed267a434dc2effd7838cd045ac3d
Victor-Alexandru <victor21cuciureanu@gmail.com>
2020-03-19 18:45:19 +0200

    all things must get and work now

--- moodle_ui/lib/data/rest_ds.dart
+++ moodle_ui/lib/data/rest_ds.dart
@@ -1,23 +1,32 @@
 import 'dart:async';
-
+import 'dart:convert';
 import 'package:moodle_ui/utils/network_util.dart';
 import 'package:moodle_ui/models/user.dart';
 
 class RestDatasource {
   NetworkUtil _netUtil = new NetworkUtil();
-  static final LOGIN_URL = "http://192.168.1.108:8000/monitor/auth";
+  static final LOGIN_URL = "http://192.168.1.108:8000/api/jwtauth/token/";
   // static final _API_KEY = "somerandomkey";
 
   Future<User> login(String username, String password) {
-    return _netUtil.post(LOGIN_URL, body: {
-      // "token": _API_KEY,
-      "username": username,
-      "password": password
-    }).then((dynamic res) {
+    Map<String, String> headers = {
+      "Accept": "application/json",
+      "content-type": "application/json"
+    };
+
+    return _netUtil
+        .post(LOGIN_URL,
+            body: {
+              // "token": _API_KEY,
+              "username": username,
+              "password": password
+            },
+            headers: headers)
+        .then((dynamic res) {
+      print("herreee");
       print("&&&&&&&&&&&&&&&&&&&&&&&");
       print(res.toString());
       print("&&&&&&&&&&&&&&&&&&&&&&&");
-      if(res["error"]) throw new Exception(res["error_msg"]);
       return new User.map(res["user"]);
     });
   }
--- moodle_ui/lib/utils/network_util.dart
+++ moodle_ui/lib/utils/network_util.dart
@@ -27,7 +27,7 @@ class NetworkUtil {
         .then((http.Response response) {
       final String res = response.body;
       final int statusCode = response.statusCode;
-
+        
       if (statusCode < 200 || statusCode > 400 || json == null) {
         throw new Exception("Error while fetching data");
       }
