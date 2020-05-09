0f7082a019517900efe825b3fede1a141d06bab6
Victor-Alexandru <victor21cuciureanu@gmail.com>
2020-05-09 23:53:38 +0300

    root added

--- moodle_ui/lib/routes.dart
+++ moodle_ui/lib/routes.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:moodle_ui/screens/home/home_page.dart';
 import 'package:moodle_ui/screens/login/login_screen.dart';
 
 final routes = {
   '/login':         (BuildContext context) => new LoginScreen(),
-  // '/home':         (BuildContext context) => new HomePage(),
   '/' :          (BuildContext context) => new LoginScreen(),
 };
