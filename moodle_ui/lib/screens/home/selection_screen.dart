ab3f638b5917ff7f9d265f2764edf3544631558c
Victor-Alexandru <victor21cuciureanu@gmail.com>
2020-05-17 01:34:05 +0300

    ui for 3 pages added

--- moodle_ui/lib/screens/home/selection_screen.dart
+++ moodle_ui/lib/screens/home/selection_screen.dart
@@ -20,102 +20,141 @@ class SelectionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return new SafeArea(
         child: Scaffold(
-      backgroundColor: Colors.redAccent,
-      body: new Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 100,
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => HomePage(_webservice)));
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.black)),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 1.5,
-                child: Text(
-                  "Find Nearby Users",
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
+            backgroundColor: Colors.redAccent,
+            body: SafeArea(
+              child: Column(children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height /
+                      3, // Also Including Tab-bar height.
+//                        child: Chewie(
+//                          controller: _chewieController,
+//                        ),
+                  child: new Center(
+                      child: new Icon(
+                    Icons.border_clear,
+                    size: MediaQuery.of(context).size.width / 2,
+                  )),
                 ),
-              ),
-              color: Colors.white,
-              textColor: Colors.black,
-              padding: EdgeInsets.all(8.0),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => GroupScreen(_webservice)));
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.black)),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 1.5,
-                child: Text(
-                  "See your groups",
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
+                Expanded(
+                  child: new Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 100,
+                        ),
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage(_webservice)));
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.black)),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            child: Text(
+                              "Find Nearby Users",
+                              style: TextStyle(fontSize: 24),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          color: Colors.white,
+                          textColor: Colors.black,
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => GroupScreen(_webservice)));
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.black)),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            child: Text(
+                              "See your groups",
+                              style: TextStyle(fontSize: 24),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          color: Colors.white,
+                          textColor: Colors.black,
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) => FirebaseScreen()));
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.black)),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            child: Text(
+                              "Firebase Notifications",
+                              style: TextStyle(fontSize: 24),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          color: Colors.white,
+                          textColor: Colors.black,
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UserProfileView(_webservice)));
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.black)),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            child: Text(
+                              "Your profile",
+                              style: TextStyle(fontSize: 24),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          color: Colors.white,
+                          textColor: Colors.black,
+                          padding: EdgeInsets.all(8.0),
+                        ), MaterialButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UserProfileView(_webservice)));
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.black)),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            child: Text(
+                              "Logout",
+                              style: TextStyle(fontSize: 24),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          color: Colors.white,
+                          textColor: Colors.black,
+                          padding: EdgeInsets.all(8.0),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-              color: Colors.white,
-              textColor: Colors.black,
-              padding: EdgeInsets.all(8.0),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => FirebaseScreen()));
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.black)),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 1.5,
-                child: Text(
-                  "Firebase Notifications",
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              color: Colors.white,
-              textColor: Colors.black,
-              padding: EdgeInsets.all(8.0),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => UserProfileView(_webservice)));
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.black)),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 1.5,
-                child: Text(
-                  "Your profile",
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              color: Colors.white,
-              textColor: Colors.black,
-              padding: EdgeInsets.all(8.0),
-            )
-          ],
-        ),
-      ),
-    ));
+              ]),
+            )));
   }
 }
